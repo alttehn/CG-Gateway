59ed7b949027cb610a3497bc56a423ccd41b715a
alttehn <31592612+alttehn@users.noreply.github.com>
2017-09-13 15:07:29 +0300

    v1

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ if (empty($_SERVER['HTTP_HOST'])) {
 // Example url (SSO script on subdomain): "a.firstsite.com"
 // Example url (SSO script in the Drupal directory): "firstsite.com/sso.php"
 $network = array(
-  'gateway.192.168.99.100.nip.io',
   'login-dev.192.168.99.100.nip.io',
   'login.192.168.99.100.nip.io',
 );
